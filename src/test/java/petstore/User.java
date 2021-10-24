7c98684a511aaec1aa713af8da870ca10a02ab51
roger.santos <roger.rezende@gmail.com>
2021-10-24 01:14:55 -0300

    Criação do teste para login do User

--- src/test/java/petstore/User.java
+++ src/test/java/petstore/User.java
@@ -1,5 +1,6 @@
 package petstore;
 
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -7,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
 public class User {
@@ -16,7 +18,7 @@ public class User {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
+    @Test(priority = 1)
     public void incluirUser() throws IOException {
         String jsonBody = lerJson("data/user1.json");
 
@@ -31,6 +33,24 @@ public class User {
                 .statusCode(200)
                 .body("type", is("unknown"))
                 .body("code", is(200))
+        ;
+    }
+
+    @Test(priority = 2)
+    public void login() {
+        given()
+                .contentType("application/json")
+                .log().all()
+                .pathParam("username", "batman")
+                .pathParam("password", "Batman123")
+        .when()
+                .get(uri + "/login?" + "username={username}" + "&" + "password={password}")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("type", is("unknown"))
+                .body("code", is(200))
+                .body("message", containsString("logged in user session"))
         ;
     }
 }
